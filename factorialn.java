91980acfcdf0eb71b01e33ed663414476cf4edbc
shaad ali <alishaad051@gmail.com>
2025-09-01 18:32:48 +0530

    Day 2: Practice

---
+++ factorialn.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class factorialn{
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.println("hello Sablog");
+    System.out.println("please enter a number");
+    int num=input.nextInt();
+    int sum=oddsum(num);
+    System.out.println("here is the sum of all odd number: "+sum);
+
+  }
+
+  public static int oddsum(int num){
+    int i=1;
+    int sum=1;
+    while (i<=num) {
+      if (i==0) {
+        return 1;
+        }
+      sum=sum*i;
+      i++;
+      }
+    return sum;
+    }
+    
+  }
---
+++ leftparamid.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class leftparamid {
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.println("please enter a number:");
+    int num =input.nextInt();
+   // printrightpattern(num);
+    printreversepattern(num);
+  }
+
+
+   public static void printreversepattern(int num){
+    int rows=num;
+    while (rows>0) {
+      System.out.print("*");
+      int i=1;
+      while (i<rows) {
+        System.out.print("*");
+        i++;
+      }
+      System.out.println();
+      rows--;
+    }
+   }
+
+
+
+
+
+
+
+
+  public static void printrightpattern(int num){
+    int rows=0;
+    while (rows<num) {
+      System.out.print("*");
+      int i=0;
+      while (i<rows) {
+        System.out.print("*");
+        i++;
+      }
+      System.out.println();
+      rows++;
+      
+    }
+  }
+
+}
