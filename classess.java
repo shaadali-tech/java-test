69262fce7a3300e76167a455d325416b3abdcc45
Shaad Ali <215ics043@gbu.ac.in>
2025-09-10 17:30:36 +0530

    Daily update

---
+++ classess.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+ class classess {
+    int num;
+    
+    String color;
+    String howzkhaas;
+
+
+
+
+    public static void main(String[] args) {
+      classess cla=new classess();
+      System.out.println(cla.color);
+
+    }
+  }
+
